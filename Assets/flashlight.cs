16c3d19c95b2d05ca573577c3024d224af779b1d
Tero <t6hote00@students.oamk.fi>
2019-05-15 14:34:27 +0300

    flashlight

---
+++ Assets/flashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    private Light lt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            lt.enabled = !lt.enabled;
+        }
+    }
+}
