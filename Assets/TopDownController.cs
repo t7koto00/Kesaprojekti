82dabc266b277525351ed8f3011e4e05e4210b34
Toni <t7koto00@students.oamk.fi>
2019-05-22 14:35:04 +0300

    Flashlight script, sprint, stamina
    And slippery removal

--- Assets/TopDownController.cs
+++ Assets/TopDownController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class TopDownController : MonoBehaviour
 {
-
     Rigidbody rigidBody;
     Camera cam;
-    public float speed = 4;
+    public float speed = 5;
+    public float sprintSpeed = 8;
+    public double stamina;
+    private double staminaStart;
     Vector3 lookPos;
+    bool playing = false;
+    AudioSource audioSource;
+    public AudioClip outOfBreath;
+
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         cam = GetComponentInChildren<Camera>();
+        staminaStart = stamina - 0.5;
+        audioSource = GetComponent<AudioSource>();
     }
     
     void Update()
@@ -37,9 +45,38 @@ public class TopDownController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(horizontal, 0, vertical);
 
-        rigidBody.AddForce(movement * speed / Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rigidBody.velocity = movement * sprintSpeed;
+            if (stamina <= 0)
+            {
+                stamina = 0;
+                if (playing != true)
+                {
+                    audioSource.clip = outOfBreath;
+                    audioSource.Play();
+                    playing = true;
+                }
+            }
+            else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                if (!audioSource.isPlaying)
+                { playing = false; }
+
+                stamina = stamina - 1;
+            }
+        }
+        else
+        {
+            rigidBody.velocity = movement * speed;
+            if (stamina <= staminaStart)
+            {
+                stamina = stamina + 0.5;
+            }
+        }
+        Debug.Log(stamina);
+
     }
 }
--- Assets/flashlight.cs
+++ Assets/flashlight.cs
@@ -10,7 +10,7 @@ public class flashlight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lt = GetComponent<Light>();
+        lt = GetComponentInChildren<Light>();
     }
 
     // Update is called once per frame
