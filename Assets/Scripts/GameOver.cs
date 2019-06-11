9734008225cbb76dbd59276ff9fdcc27ea3848a0
Toni <t7koto00@students.oamk.fi>
2019-06-11 13:55:04 +0300

    Kiinnijäänti ääni gameover ruutuun

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
+    AudioSource audioSource;
+    public AudioClip caught;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = caught;
+        audioSource.Play();
+    }
     public void TryAgain()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
