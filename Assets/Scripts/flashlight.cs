3e970c2e9afdd744c7be7bba490aa68b67cf200f
Tero <t6hote00@students.oamk.fi>
2019-06-14 13:34:39 +0300

    patteri toiminto taskulampulle

--- Assets/Scripts/flashlight.cs
+++ Assets/Scripts/flashlight.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class flashlight : MonoBehaviour
 {
     private Light lt;
     AudioSource audioData;
     public AudioClip onSound;
+    private double battery = 50;
+    public Slider BatterySlider;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,24 @@ public class flashlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BatterySlider.value = (float)battery;
+        if (lt.enabled == true)
+        {
+            battery = battery - 0.1;
+        }
+        if(lt.enabled == false)
+        {
+            battery = battery + 0.05;
+        }
+        if (battery <= 0)
+        {
+            lt.enabled = false;
+        }
+        if (battery > 100)
+        {
+            battery = 100;
+        }
+        Debug.Log(battery);
         if (Input.GetKeyDown(KeyCode.F))
         {
             lt.enabled = !lt.enabled;
