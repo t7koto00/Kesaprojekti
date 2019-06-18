61aa3f2a4daded3c312163c065c2cdf835887bc9
Tero <t6hote00@students.oamk.fi>
2019-06-18 15:44:11 +0300

    Mainmenun parantelua

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,37 +6,8 @@ using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
-    AudioSource audioSource;
-    public AudioClip music;
     public GameObject Optionsmenu;
     public GameObject Mainmenu;
-    //public AudioMixer mixer;
-    //public Slider slider;
-    //float volume = 100;
-
-    void Start()
-    {
-        //slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
-        //slider.value = volume;
-        //mixer.SetFloat("Master", volume);
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = music;
-        audioSource.Play();
-    }
-
-    /*void FixedUpdate()
-    {
-        volume = slider.value;
-        mixer.SetFloat("Master", volume);
-        Debug.Log(volume);
-    }*/
-
-   /* public void SetLevel(float sliderValue)
-    {
-        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
-        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
-        Debug.Log(sliderValue);
-    }*/
 
     public void PlayGame()
     {
